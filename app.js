66424f283c660ec3ebd81363215977f9f40d5337
Abhishek Kumar <vermaabhishek128@gmail.com>
2023-03-05 00:02:20 +0530

    added

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express()
 const cors = require("cors")
 const port = process.env.port || 5000;
 app.use(cors())
-
+mongoose.set("strictQuery",false)
 const DB = 'mongodb+srv://parthThacker:parth1213@cluster0.twdoy22.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(DB).then(()=>{console.log("Connected");}).catch((err)=>{console.log(err.message);})
 //app.use(require("./route/Route"))

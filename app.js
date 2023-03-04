25cb7edb14d7a5259129ed88254f0d05ded4397c
Abhishek Kumar <vermaabhishek128@gmail.com>
2023-03-04 23:52:03 +0530

    adedd

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const user = require("./routes/user")
 const proposal = require('./routes/proposals')
 const app = express()
 const cors = require("cors")
-
+const port = process.env.port || 8080;
 app.use(cors())
 
 const DB = 'mongodb+srv://parthThacker:parth1213@cluster0.twdoy22.mongodb.net/?retryWrites=true&w=majority'
@@ -20,26 +20,4 @@ app.get("/",(req,res)=>{
     res.send("ok")
 })
 
-app.listen(8080,()=>{console.log("Server is up")})
-
-// const express = require("express")
-// const mongoose = require("mongoose")
-// const vendor = require("./routes/vendor")
-// const user = require("./routes/user")
-// const app = express()
-// const cors = require("cors")
-// app.use(cors())
-
-
-// const DB = 'mongodb+srv://parthThacker:parth1213@cluster0.twdoy22.mongodb.net/?retryWrites=true&w=majority'
-// mongoose.connect(DB).then(()=>{console.log("Connected");}).catch((err)=>{console.log(err.message);})
-
-// app.use(express.json())
-// app.use("/vendor",vendor)
-// app.use("/",user)
-
-// app.get("/",(req,res)=>{
-//     res.send("ok")
-// })
-
-// app.listen(8080,()=>{console.log("Server is up")})
+app.listen(port,()=>{console.log("Server is up")})
